c77c2cb0db42a07c0e86f8eaa87d9b40bf8c646a
disha3604 <disha3604@gmail.com>
2023-03-02 16:04:47 +0500

    v1

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+var user = prompt(' Укажите свое имя ');
+var age = prompt( 'Укажите свой возрост ')
+
+console.log(' Меня зовут ' + user);
+console.log(' Мне ' + age);
+
+var minus = prompt('25-5= ')
+alert('Пример 1 :   '+' 25-5= 20  ' + ' Ваш ответ  ' + minus)
+
+var plus = prompt('25+5= ')
+alert('Пример 2 :   ' +'25+5= 30  '+' Ваш ответ  ' + plus )
+
+var umnojenie = prompt('5*5= ')
+alert('Пример 3 :   ' +'5*5= 25  ' + ' Ваш ответ  ' + umnojenie)  
+
+var delenie = prompt('25 / 5= ')
+alert('Пример 4 :  ' + '25 / 5= 5  ' + ' Ваш ответ  ' + delenie)
+
+
+var x = prompt('Введитье первое число ');
+var y = prompt('Введитье второе число ');
+var z = prompt('Введитье третие  число ');
+var res = (x+y+z)/3;
+
+
+alert('Среднее арифметическое число =' + res )
